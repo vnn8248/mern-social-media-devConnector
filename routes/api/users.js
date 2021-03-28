556c13fe891a270f6846a093b3288a96bb342361
Bibi Westside <vnn8248@gmail.com>
2021-03-28 11:30:53 -0400

    Add and config express validator to user route. Test errors and response with postman

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 // @route   POST api/users
 // @desc    Test route
 // @access  Public
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("Users route")
-});
+router.post("/",
+  check("name", "Name is required").not().isEmpty(),
+  check("email", "Must be a valid email").isEmail(),
+  check("password", "Must be at least 5 characters").isLength({ min: 5 }),
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
+    res.send("User route");
+  }
+);
 
 module.exports = router;
