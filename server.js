4ebbdf8c8cc98028f7ef6892322fdb87a8cd1290
Bibi Westside <vnn8248@gmail.com>
2021-03-27 21:37:49 -0400

    First commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Ahoy!")
+});
+
+let PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+});
