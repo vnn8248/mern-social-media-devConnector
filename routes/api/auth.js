0b22d5b9f8d263bbb62a6d35e878dd46ddc50d26
Bibi Westside <vnn8248@gmail.com>
2021-03-28 15:42:36 -0400

    Create middleware to verify token, add middleware to auth route and return authenticated user info

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const User = require("../../models/User");
 
 // @route   GET api/auth
-// @desc    Test route
+// @desc    Get authenticated users
 // @access  Public
-router.get("/", (req, res) => { res.send("Auth route") });
+router.get("/", auth, async (req, res) => {
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Authentication failed." });
+  }
+});
 
 module.exports = router;
