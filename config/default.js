e81614d1f668113affa660935d1761680f2fdf3f
Bibi Westside <vnn8248@gmail.com>
2021-03-29 13:45:24 -0400

    Add endpoint for github repos in user profile

--- config/default.js
+++ config/default.js
@@ -2,7 +2,9 @@ require('dotenv').config()
 
 const configs = {
   mongoURI: process.env.MONGO_URI,
-  jwtSecret: process.env.JWTSECRET
+  jwtSecret: process.env.JWTSECRET,
+  githubClientId: process.env.GITHUB_CLIENT_ID,
+  githubClientSecret: process.env.GITHUB_CLIENT_SECRET
 };
 
 module.exports = configs;
--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
 const { check, validationResult } = require("express-validator");
+const request = require("request");
+const config = require("config");
 
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
@@ -292,6 +294,32 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
     await profile.save();
 
     res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+// @route   GET api/github/:username
+// @desc    Get repositories from GitHub username
+// @access  Public
+router.get("/github/:username", async (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubClientSecret')}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: "No GitHub profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ msg: "Server error" });
