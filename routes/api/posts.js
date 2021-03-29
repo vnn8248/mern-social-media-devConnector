0a065cd3612e853df18feb7f7391c9d8d823b87c
Bibi Westside <vnn8248@gmail.com>
2021-03-29 16:48:06 -0400

    Create post model and endpoint to add posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const router = express.Router();
+const { check, validateResult, validationResult } = require("express-validator");
+const auth = require("../../middleware/auth");
 
-// @route   GET api/posts
-// @desc    Test route
+const Post = require("../../models/Post");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+
+// @route   POST api/posts
+// @desc    Create a post
 // @access  Public
-router.get("/", (req, res) => { res.send("Posts route") });
+router.post("/", [
+  auth,
+  check("text", "Text is required").not().isEmpty()
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) res.status(500).json({ msg: "Server error" });
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    });
+
+    await newPost.save();
+
+    res.json(newPost);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
 
 module.exports = router;
