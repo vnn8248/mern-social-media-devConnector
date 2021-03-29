b616a5977e268c0a4bdfbb307dfb6fb89291d0c9
Bibi Westside <vnn8248@gmail.com>
2021-03-29 10:31:57 -0400

    Create delete user and profile endpoints

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -137,6 +137,25 @@ router.get("/user/:user_id", async (req, res) => {
   }
 });
 
+// @route   DELETE api/profiles
+// @desc    Delete profile, user, and posts
+// @access  Private
+router.delete("/", auth, async (req, res) => {
+  try {
+
+    // Remove profile
+    await Profile.findOneAndRemove({ user: req.user.id });
+
+    // Remove user
+    await User.findOneAndRemove({ _id: req.user.id });
+
+    res.json({ msg: "User and profile removed" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
 
 
 module.exports = router;
