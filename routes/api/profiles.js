feef6c2ccec229d3a5d3b49770df759c134993f4
Bibi Westside <vnn8248@gmail.com>
2021-03-29 10:12:30 -0400

    Add get all profiles endpoint and get profile by user id endpoint

--- routes/api/profiles.js
+++ routes/api/profiles.js
@@ -100,4 +100,43 @@ router.post("/", [
   }
 });
 
+// @route   GET api/profiles
+// @desc    Get all profiles
+// @access  Public
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+// @route   GET api/profiles/user/:user_id
+// @desc    Get a profile by user ID
+// @access  Public
+router.get("/user/:user_id", async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.user_id }).populate("user", ["name", "avatar"]);
+
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === "ObjectId") {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+
+
 module.exports = router;
