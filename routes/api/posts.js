3b61061f28bc6ce68537327aacc9c13e42461667
Bibi Westside <vnn8248@gmail.com>
2021-03-30 10:04:40 -0400

    Create endpoints for adding and removing comments

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,7 +9,7 @@ const User = require("../../models/User");
 
 // @route   POST api/posts
 // @desc    Create a post
-// @access  Public
+// @access  Private
 router.post("/", [
   auth,
   check("text", "Text is required").not().isEmpty()
@@ -126,6 +126,72 @@ router.put("/unlike/:post_id", auth, async (req, res) => {
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ msg: "Server Error" });
+  }
+});
+
+// @route   POST api/posts/comment/:post_id
+// @desc    Create a comment
+// @access  Private
+router.post("/comment/:post_id", [
+  auth,
+  check("text", "Text is required").not().isEmpty()
+], async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) res.status(500).json({ msg: "Server error" });
+
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    const post = await Post.findById(req.params.post_id);
+
+    const newComment = {
+      text: req.body.text,
+      name: user.name,
+      avatar: user.avatar,
+      user: req.user.id
+    };
+
+    post.comments.push(newComment);
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
+  }
+});
+
+// @route   DELETE api/posts/comment/:post_id/:comment_id
+// @desc    Delete a comment
+// @access  Private
+router.delete("/comment/:post_id/:comment_id", auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.post_id);
+
+    // Find comment
+    const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+    if (!comment) {
+      return res.status(404).json({ msg: "Comment does not exist" });
+    }
+
+    // Check user
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: "User not authorized" });
+    }
+
+    // Get index to remove
+    const removeIndex = post.comments.map(comment => comment.user.toString()).indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ msg: "Server error" });
   }
 });
 
