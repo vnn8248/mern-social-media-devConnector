520a23abd593c0862951d39cef353fb36ea1474e
Bibi Westside <vnn8248@gmail.com>
2021-04-06 15:43:01 -0400

    Create dashboard component, get profile action, profile reducer, connect component to redux, get profile in dashboard state

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from "react-redux";
+import { getCurrentProfile } from "../../actions/profile";
 
-const Dashboard = props => {
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []);
+
   return (
     <div>
       Dashboard
     </div>
   )
-}
+};
 
 Dashboard.propTypes = {
+  auth: PropTypes.object.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-}
+const mapStateToProps = state => ({
+  auth: state.auth,
+  profile: state.profile
+});
 
-export default Dashboard
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
