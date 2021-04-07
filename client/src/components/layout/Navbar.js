76438f8bfa72b00e751057456b9585c5a64973f5
Bibi Westside <vnn8248@gmail.com>
2021-04-07 11:08:34 -0400

    Render user info on dashboard after successful login. Direct users who don't have a profile to create one

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,6 +8,12 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/dashboard">
+          <i className="fa fas fa-user"></i>{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href="#!">
           <i className="fa fas fa-sign-out-alt"></i>{" "}
           <span className="hide-sm">Logout</span>
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { GET_PROFILE, PROFILE_ERROR } from "../actions/types";
+import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE } from "../actions/types";
 
 
 const initialState = {
@@ -24,6 +24,13 @@ export default function (state = initialState, action) {
       return {
         ...state,
         error: payload,
+        loading: false
+      }
+    case CLEAR_PROFILE:
+      return {
+        ...state,
+        profile: null,
+        repos: [],
         loading: false
       }
     default:
