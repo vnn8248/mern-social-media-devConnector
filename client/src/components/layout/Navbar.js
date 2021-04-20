f7e3b47ba3e7b6eb3a86949f4353da0d7637d64b
Bibi Westside <vnn8248@gmail.com>
2021-04-20 16:12:11 -0400

    Create post reducer, get all posts action, post component, post item component

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -13,6 +13,11 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
         </Link>
       </li>
       <li>
+        <Link to="/posts">
+          Posts
+        </Link>
+      </li>
+      <li>
         <Link to="/dashboard">
           <i className="fa fas fa-user"></i>{" "}
           <span className="hide-sm">Dashboard</span>
---
+++ client/src/reducers/post.js
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_POSTS, POST_ERROR } from '../actions/types';
+
+const initialState = {
+  posts: [],
+  post: null,
+  loading: true,
+  error: {}
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: payload,
+        loading: false
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload
+      };
+    default:
+      return state;
+  }
+};
